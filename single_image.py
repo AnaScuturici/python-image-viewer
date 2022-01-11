7e7728c36ae9dd037400ee1434883ab11351d54d
ana scuturici <anascuturici@gmail.com>
2022-01-11 16:42:03 +0900

    Refactor: resize image maintaining ratio

--- single_image.py
+++ single_image.py
@@ -9,12 +9,23 @@ root.geometry("800x500")
 img = Image.open("img/IMG_6870.JPG")
 
 # Resize img
-resized = img.resize((350, 400), Image.ANTIALIAS)
 
-resized_img = ImageTk.PhotoImage(resized)
+"""
+print(img)  
+<PIL.JpegImagePlugin.JpegImageFile image mode=RGB size=768x1024 at 0x10DF1D450>
+"""
 
-# Img size
-label = Label(root, image=resized_img)
+def resize_img(img, new_width):
+    width, height = img.size
+    ration = height / width
+    new_height = int(ration * new_width)
+    resized_img = img.resize((new_width, new_height), Image.ANTIALIAS)
+    return resized_img
+
+resized = resize_img(img, 300)
+new_img = ImageTk.PhotoImage(resized)
+
+label = Label(root, image=new_img)
 label.pack(pady=20)
 
 exit_button = Button(root, text="Exit", command=root.quit)
