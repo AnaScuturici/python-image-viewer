ee3060ed02e30c38e16ffa811c64e5c3043de544
ana scuturici <anascuturici@gmail.com>
2022-01-12 16:10:17 +0900

    Fix missing lines

--- image_list.py
+++ image_list.py
@@ -32,65 +32,60 @@ img5 = ImageTk.PhotoImage(Image.open("resized-img/IMG_6887.JPG"))
 
 image_list = [img1, img2, img3, img4, img5]
 
-label = Label(root, image=img1)
+def forward(img_no):
+    global label
+    global forward_button
+    global exit_button
+    global back_button
+
+    # clear screen
+    label.grid_forget()
+
+    label = Label(image=image_list[img_no-1])
+    label.grid(row=0, column=0, columnspan=3)
+    forward_button = Button(root, text=">>", command=lambda: forward(img_no + 1))
+
+    if img_no == 5:
+        forward_button = Button(root, text=">>", state=DISABLED)
+
+    back_button = Button(root, text="<<", command=lambda: back(img_no - 1))
+
+    back_button.grid(row=1, column=0)
+    exit_button.grid(row=1, column=1)
+    forward_button.grid(row=1, column=2)
+
+def back(img_no):
+    global label
+    global forward_button
+    global exit_button
+    global back_button
+
+    label.grid_forget()
+
+    label = Label(image=image_list[img_no - 1])
+    label.grid(row=0, column=0, columnspan=3)
+    forward_button = Button(root, text=">>", command=lambda: forward(img_no + 1))
+    back_button = Button(root, text="<<", command=lambda: back(img_no - 1))
+
+    if img_no == 1:
+        back_button = Button(root, text="<<", state=DISABLED)
+
+    label.grid(row=0, column=0, columnspan=3)
+    back_button.grid(row=1, column=0)
+    exit_button.grid(row=1, column=1)
+    forward_button.grid(row=1, column=2)
+
+
+label = Label(image=img1)
 label.grid(row=0, column=0, columnspan=3)
 
-back_button = Button(root, text="<<", command=lambda: back(3))
+back_button = Button(root, text="<<", command=back, state=DISABLED)
 exit_button = Button(root, text="Exit", command=root.quit)
 forward_button = Button(root, text=">>", command=lambda: forward(2))
 
 back_button.grid(row=1, column=0)
 exit_button.grid(row=1, column=1)
 forward_button.grid(row=1, column=2)
-
-def forward(img_num):
-    """
-    global label
-    global forward_button
-    global back_button
-    """
-
-    label.configure(image=image_list[img_num + 1])
-    #delete the currently displayed photo
-    """
-    label.grid_forget()
-    # recreate all widgets
-    label = Label(image=image_list[img_num - 1])
-    forward_button = Button(root, text=">>", command=lambda: forward_button(img_num + 1))
-    back_button = Button(root, text="<<", command=lambda: back_button(img_num - 1))
-    
-    # stop after last photo
-    if img_num == 5:
-        forward_button = Button(root, text=">>", state=DISABLED)
-
-    label.grid(row=0, column=0, columnspan=3)
-    back_button.grid(row=1, column=0)
-    forward_button.grid(row=1, column=2)
-    """
-
-def back(img_num):
-    """
-    global label
-    global forward_button
-    global back_button
-    """
-
-    label.configure(image=image_list[img_num - 1])
-
-    """
-    label.grid_forget()
-
-    label = Label(image=image_list[img_num + 1])
-    forward_button = Button(root, text=">>", command=lambda: forward_button(img_num + 1))
-    back_button = Button(root, text="<<", command=lambda: back_button(img_num - 1))
-
-    if img_num == 1:
-        back_button = Button(root, text="<<", state=DISABLED)
-
-    label.grid(row=0, column=0, columnspan=3)
-    back_button.grid(row=1, column=0)
-    forward_button.grid(row=1, column=2)
-    """
 
 
 root.mainloop()
